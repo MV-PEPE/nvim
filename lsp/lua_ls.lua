7e01d4fe40de22dd414aedc577b8f055764edc4c
MV-PEPE <maksvas03@gmail.com>
2025-11-04 22:40:18 +0100

    Add a root marker for lua LSP

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,6 +1,6 @@
 return {
     cmd = { 'lua-language-server' },
     filetypes = { 'lua' },
-    root_markers = { '.git' },
+    root_markers = { '.git', '.luarc.json' },
     single_file_support = true
 }
