e5c678a8426858ef9a28247a54b19ebe0783d47e
MV-PEPE <maksvas03@gmail.com>
2025-11-04 22:39:53 +0100

    Add LSP keymaps for code actions and definitions

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,9 @@ vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 
 -- Map Shift-k to display documentation in a floating window
 vim.keymap.set('n', '<S-k>', vim.lsp.buf.hover, {})
+
+-- Map F12 to go to the definition
+vim.keymap.set('n', '<F12>', vim.lsp.buf.definition, {})
+
+-- Map code actions to Space-ca
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
