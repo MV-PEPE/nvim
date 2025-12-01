f408100124522c42e4737588266d26ed2db8865b
MV-PEPE <maksvas03@gmail.com>
2025-12-01 23:29:37 +0100

    Remap the file finder

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,9 @@ vim.keymap.set('n', '<S-e>', ':Neotree<CR>', {})
 -- Map Space-n to open/close the file tree on the left
 vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left toggle<CR>', {})
 
--- Map Ctrl-P to open the file finder
+-- Map Space-f to open the file finder
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 
 -- Map Shift-k to display documentation in a floating window
 vim.keymap.set('n', '<S-k>', vim.lsp.buf.hover, {})
