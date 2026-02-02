f007db2eb4fa19a1e58ff314196b0930ae5e5fe6
MV-PEPE <maksvas03@gmail.com>
2026-02-02 14:35:25 +0100

    Add the default keys' setup explicitly

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,7 +1,23 @@
 -- Automatically close brackets
 return {
     'm4xshen/autoclose.nvim',
+
     config = function()
-        require("autoclose").setup()
+        require("autoclose").setup({
+            keys = {
+                ["("] = { escape = false, close = true, pair = "()" },
+                ["["] = { escape = false, close = true, pair = "[]" },
+                ["{"] = { escape = false, close = true, pair = "{}" },
+
+                [">"] = { escape = true, close = false, pair = "<>" },
+                [")"] = { escape = true, close = false, pair = "()" },
+                ["]"] = { escape = true, close = false, pair = "[]" },
+                ["}"] = { escape = true, close = false, pair = "{}" },
+
+                ['"'] = { escape = true, close = true, pair = '""' },
+                ["'"] = { escape = true, close = true, pair = "''" },
+                ["`"] = { escape = true, close = true, pair = "``" },
+            },
+        })
     end
 }
